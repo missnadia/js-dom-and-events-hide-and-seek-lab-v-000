4abc1092fef5f019b81f1634d7d80c3264b73067
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-05 18:32:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list')
+  const rankedLists = document.querySelectorAll('.ranked-list');
 
   for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children
+    let children = rankedLists[i].children;
 
     for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
     }
-  }
+  };
 }
 
 function deepestChild() {
