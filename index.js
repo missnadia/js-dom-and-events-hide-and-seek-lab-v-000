7e4e78fbc8f75e9f2725b8e989dc88fcb00d86d4
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-05 18:26:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,19 +13,12 @@ function increaseRankBy (n) {
     let children = rankedLists[i].children;
 
     for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML parseInt(children[j].innerHTML) + n
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
     }
   }
 }
 
 function deepestChild() {
   let node = document.getElementById('grand-node')
-let nextNode = node.children[0]
 
-while (nextNode) {
-  node = nextNode
-  nextNode = node.children[0]
-}
-
-return node
 }
