242260b1a1d868872630d8591a68af1994d9ec3e
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-05 18:27:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-function increaseRankBy (n) {
+function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list');
 
   for (let i = 0, l = children.length; i < l; i++) {
-    let children = rankedLists[i].children;
+    let children = rankedLists[i].children
 
     for (let j = 0, k = children.length; j < k; j++) {
       children[j].innerHTML = parseInt(children[j].innerHTML) + n
