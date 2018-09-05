6b77b8f3c9976d9eede12c84aa8349dc23b0c570
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-05 18:24:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,13 @@ function increaseRankBy (n) {
 }
 
 function deepestChild() {
-  return document.getElementById('grand-node');
+  let node = document.getElementById('grand-node')
+let nextNode = node.children[0]
+
+while (nextNode) {
+  node = nextNode
+  nextNode = node.children[0]
+}
+
+return node
 }
